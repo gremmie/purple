7deb24acbf262e1a9cb9e22d0bf93ab9c61a6f3d
Brian Neal <gremmie@users.noreply.github.com>
2014-02-18 19:27:37 -0600

    Added __version__ constant.

---
+++ purple/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (C) 2014 by Brian Neal.
+# This file is part of purple, the PURPLE (Cipher Machine 97) simulation.
+# purple is released under the MIT License (see LICENSE.txt).
+
+__version__ = '0.1.0'
